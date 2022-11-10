2fae5b10577ca354fdc999ad2893d09ef1b8b16f
VimRyan <greggry.1616085@gmail.com>
2022-11-10 14:44:17 -0800

    File reading is done, need to add functionality to buttons

--- Neural Network/Form1.Designer.cs
+++ Neural Network/Form1.Designer.cs
@@ -23,12 +23,160 @@
         ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(46, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(164, 101);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 56);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(108, 24);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "Test Images";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 26);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(114, 24);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Train images";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(299, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Epochs:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(235, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Current Accuracy:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(37, 168);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(150, 27);
+            this.numericUpDown1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(299, 278);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Prediction: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(299, 249);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Image Truth: ";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(37, 213);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(256, 249);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(299, 338);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 54);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Start Training";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(299, 410);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(158, 54);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Make Prediction";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(642, 507);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private GroupBox groupBox1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private Label label1;
+        private Label label2;
+        private NumericUpDown numericUpDown1;
+        private Label label3;
+        private Label label4;
+        private PictureBox pictureBox1;
+        private Button button1;
+        private Button button2;
     }
 }
--- Neural Network/Form1.cs
+++ Neural Network/Form1.cs
@@ -1,7 +1,155 @@
 namespace Neural_Network {
+    
+    
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
+            neuralNetwork network;
+
+            //this will load two 2d arrays with the image pixel data
+            trainImages = OpenByteFile(trainImagePath, trainMax);
+            testImages = OpenByteFile(testImagePath, testMax);
+
+            trainLabels = openLabelFile(trainLabelsPath, trainMax);
+            testLabels = openLabelFile(testLabelsPath, testMax);
+
+            numericUpDown1.Maximum = trainMax - 1;
+
+            displayImage(radioButton1.Checked ? true : false, (int)numericUpDown1.Value);
+            numericUpDown1.Maximum = trainMax - 1;
+        }
+
+        private const int trainMax = 60_000;
+        private const int testMax = 10_000;
+
+        //Theses are the file paths to access the mnist images
+        //Image files contain the images themselves
+        //Label files contain what the value of each picture is
+        private const String testImagePath = "..\\..\\..\\t10k-images.idx3-ubyte";
+        private const String trainImagePath = "..\\..\\..\\train-images.idx3-ubyte";
+        private const String testLabelsPath = "..\\..\\..\\t10k-labels.idx1-ubyte";
+        private const String trainLabelsPath = "..\\..\\..\\train-labels.idx1-ubyte";
+
+        //Arrays that will store all test and training images and labels
+        private int[,] trainImages = new int[trainMax, 28 * 28];
+        private int[,] testImages = new int[testMax, 28 * 28];
+        private int[] trainLabels = new int[trainMax];
+        private int[] testLabels = new int[testMax];
+
+        int imageTruth;
+        int epochs = 0;
+        int[] currentImage = new int[784];
+
+        /**
+        * This function will open the the image file the contains the data for the images.
+        * The file does not contain an image, it contains the values of each pixel for each image.
+        * It reads in the file and returns a 2d array that contains the data
+        * data[0,...] contains the values for image 0
+        * data[1,...] contains the values for image 1
+        */
+        private int[,] OpenByteFile(String filePath, int imageAmount) {
+
+            byte[] temp;
+            int[,] data = new int[imageAmount, 28 * 28];
+            int offset = 0;
+
+            BinaryReader images = new BinaryReader(new FileStream(filePath, FileMode.Open));
+
+            images.ReadBytes(16);
+            temp = images.ReadBytes((int)images.BaseStream.Length);
+            // textBox1.Text += filePath + imageAmount + "             ";
+
+            for (int x = 0; x < imageAmount; x++) {
+                for (int y = 0; y < 28 * 28; y++) {
+                    data[x, y] = temp[offset];
+                    offset++;
+                }
+            }
+
+            images.Close();
+            return data;
+        }
+
+        /**
+         * Opens a label file and returns an array with the labels for each image
+         * data[0] contains the label for image 0
+         * data[1] contains the label for image 1
+         */
+        private int[] openLabelFile(String filePath, int imageAmount) {
+            byte[] temp;
+            int[] data = new int[imageAmount];
+            //int offset = 0;
+
+            BinaryReader images = new BinaryReader(new FileStream(filePath, FileMode.Open));
+
+            images.ReadBytes(8);
+            temp = images.ReadBytes((int)images.BaseStream.Length);
+            //  textBox1.Text += filePath + imageAmount + "\n";
+
+            for (int i = 0; i < imageAmount; i++) {
+                data[i] = temp[i];
+            }
+
+            images.Close();
+            return data;
+        }
+
+        private void displayImage(bool imageType, int imageVal) {
+            Bitmap picImage = new Bitmap(28, 28);
+            Color pix;
+            int val = 0;
+            int[,] images;
+
+
+            if (imageType) {
+                images = trainImages;
+                imageTruth = trainLabels[imageVal];
+                label4.Text = "Image Truth: " + imageTruth.ToString();
+            } else {
+                images = testImages;
+                imageTruth = testLabels[imageVal];
+                label4.Text = "Image Truth: " + imageTruth.ToString();
+            }
+
+            for (int i = 0; i < 28; i++) {
+                for (int j = 0; j < 28; j++) {
+                    currentImage[val] = images[imageVal, val];
+                    pix = Color.FromArgb(images[imageVal, val], images[imageVal, val], images[imageVal, val]);
+                    picImage.SetPixel(j, i, pix);
+                    val++;
+                }
+            }
+            pictureBox1.Image = makeBig(makeBig(picImage));
+
+        }
+        private Bitmap makeBig(Bitmap image) {
+            Bitmap newBitmap = new Bitmap(image.Width * 3, image.Height * 3);
+            Color pix;
+
+            for (int i = 0; i < image.Width; i++) {
+                for (int j = 0; j < image.Height; j++) {
+                    pix = image.GetPixel(i, j);
+
+                    newBitmap.SetPixel((i * 3), (j * 3), pix); newBitmap.SetPixel((i * 3) + 1, (j * 3), pix); newBitmap.SetPixel((i * 3) + 2, (j * 3), pix);
+                    newBitmap.SetPixel((i * 3), (j * 3) + 1, pix); newBitmap.SetPixel((i * 3) + 1, (j * 3) + 1, pix); newBitmap.SetPixel((i * 3) + 2, (j * 3) + 1, pix);
+                    newBitmap.SetPixel((i * 3), (j * 3) + 2, pix); newBitmap.SetPixel((i * 3) + 1, (j * 3) + 2, pix); newBitmap.SetPixel((i * 3) + 2, (j * 3) + 2, pix);
+                }
+            }
+
+            return newBitmap;
+
         }
     }
+
+    class neuralNetwork {
+
+        //Default constructor
+        public neuralNetwork() {
+            
+        }
+
+
+      
+    }
+
 }
