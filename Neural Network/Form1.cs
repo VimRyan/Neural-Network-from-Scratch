a228a75bab5e836da5281fb9b650459c9cf7a548
VimRyan <greggry.1616085@gmail.com>
2022-11-12 17:57:17 -0800

    feed forward and back propagation are done

--- Neural Network/Form1.cs
+++ Neural Network/Form1.cs
@@ -6,8 +6,8 @@ namespace Neural_Network {
             InitializeComponent();
 
             //these will load 4 arrays with the image data
-            im.trainImages = OpenByteFile(trainImagePath, trainMax);
-            im.testImages = OpenByteFile(testImagePath, testMax);
+            im.trainImages = openByteFile(trainImagePath, trainMax);
+            im.testImages = openByteFile(testImagePath, testMax);
             im.trainLabels = openLabelFile(trainLabelsPath, trainMax);
             im.testLabels = openLabelFile(testLabelsPath, testMax);
 
@@ -15,7 +15,9 @@ namespace Neural_Network {
             displayImage(radioButton1.Checked ? true : false, (int)numericUpDown1.Value);
             numericUpDown1.Maximum = trainMax - 1;
 
-            network = new neuralNetwork(im);
+            //takes in an images object that has the 4 arrays with image data and the
+            // the dimensions of the images
+            network = new neuralNetwork(im, 28*28, trainMax, testMax);
         }
 
         private const int trainMax = 60_000;
@@ -43,7 +45,7 @@ namespace Neural_Network {
         * data[0,...] contains the values for image 0
         * data[1,...] contains the values for image 1
         */
-        private int[,] OpenByteFile(String filePath, int imageAmount) {
+        private int[,] openByteFile(String filePath, int imageAmount) {
 
             byte[] temp;
             int[,] data = new int[imageAmount, 28 * 28];
@@ -189,6 +191,8 @@ namespace Neural_Network {
             displayMessage("This might take a while");
             network.startEpoch();
 
+
+            label2.Text = "Epochs: " + network.epochs.ToString();
             displayMessage("Done");
         }
 
@@ -228,39 +232,217 @@ namespace Neural_Network {
     }
 
     class neuralNetwork {
-        int epochs;
+        public int epochs;
 
-        public int[,] trainImages;
-        public int[,] testImages;
-        public int[] trainLabels;
-        public int[] testLabels;
+        //these arrays store the image data
+        int[,] trainImages;
+        int[,] testImages;
+        int[] trainLabels;
+        int[] testLabels;
 
-        double[,] wO = new double[784, 50]; double[,] w = new double[784, 50];
-        double[] bO = new double[50]; double[] b = new double[50];
-        double[,] uO = new double[50, 10]; double[,] u = new double[50, 10];
-        double[] cO = new double[10]; double[] c = new double[10];
-        //   double[] zO = new double[10];      
-        double[] z = new double[10];
-        double[] s = new double[50]; double[] y = new double[50];
-        double[] r = new double[10];
-        int[] t = new int[10];
+        // Variables that dictate the amount of nodes each layer has  
+        static int layer1count = 50;
+        static int outputLayerCount = 10;
+        static int inputLayerCount;
 
-        private const double learn = 0.005;
+        int trainMax, testMax;
 
+        //arrays that are used to do the calculations
 
-        public neuralNetwork(images im) {
+        double[] inputLayer  = new double[inputLayerCount];
+        
+        // double[,] wO = new double[784, 50]; double[,] w = new double[784, 50];
+        double[,] weight1Old = new double[inputLayerCount, layer1count];
+        double[,] weight1 = new double[inputLayerCount, layer1count];
+
+        //double[] y = new double[50];
+        double[] firstLayer = new double[layer1count];
+
+        // double[] bO = new double[50]; double[] b = new double[50];
+        double[] layer1BiasOld = new double[layer1count];
+        double[] layer1Bias    = new double[layer1count];
+
+        // double[] s = new double[50]; 
+        double[] S = new double[layer1count]; 
+
+        // double[,] uO = new double[50, 10]; double[,] u = new double[50, 10];
+        double[,] weight2Old = new double[layer1count, outputLayerCount];
+        double[,] weight2    = new double[layer1count, outputLayerCount];
+
+        // double[] cO = new double[10]; double[] c = new double[10];
+        double[] outLayerBiasOld = new double[outputLayerCount];
+        double[] outLayerBias    = new double[outputLayerCount];
+
+        //double[] r = new double[10];
+        double[] outputLayer = new double[outputLayerCount];
+
+        // double[] z = new double[10];
+        double[] finalOutput = new double[outputLayerCount];
+
+        //int[] t = new int[10];
+        double[] truth = new double[outputLayerCount];
+
+        /* double[,] wO = new double[784, 50]; double[,] w = new double[784, 50];
+         double[] bO = new double[50]; double[] b = new double[50];
+         double[,] uO = new double[50, 10]; double[,] u = new double[50, 10];
+         double[] cO = new double[10]; double[] c = new double[10];
+         //   double[] zO = new double[10];      
+         double[] z = new double[10];
+         double[] s = new double[50]; double[] y = new double[50];
+         double[] r = new double[10];
+         int[] t = new int[10];
+        */
+        const double learn = 0.005;
+
+        public neuralNetwork(images im, int x, int trainM, int testM) {
             epochs = 0;
             trainImages = im.trainImages;
             testImages = im.testImages;
             trainLabels = im.trainLabels;
             testLabels = im.testLabels;
+            trainMax = trainM;
+            testMax = testM;
+            inputLayerCount = x;
+            fillArrays();
         }
 
         /*
+         * Fills the the initial arrays with values so calculations can be done
          */
-        
-        public void startEpoch() {
+        private void fillArrays() {
+            var rand = new Random();
+
+            for (int i = 0; i < inputLayerCount; i++) {
+                for (int j = 0; j < layer1count; j++) {
+                    //wO[i, j] = rand.NextDouble() - 0.5;
+                    weight1Old[i, j] = rand.NextDouble() - 0.5;
+                }
+            }
+
+            for (int i = 0; i < layer1count; i++) {
+                //bO[i] = rand.NextDouble() - 0.5;
+                layer1BiasOld[i] = rand.NextDouble() - 0.5;
+                for (int j = 0; j < outputLayerCount; j++) {
+                    //uO[i, j] = rand.NextDouble() - 0.5;
+                    weight2Old[i, j] = rand.NextDouble() - 0.5;
+                }
+            }
+
+            for (int i = 0; i < outputLayerCount; i++) {
+                //cO[i] = rand.NextDouble() - 0.5;
+                outLayerBiasOld[i] = rand.NextDouble() - 0.5;
             
+            }
+        }
+
+
+        /*
+         * Calculates one Epoch
+         * Includes feed foward and back propagation
+         */
+        public void startEpoch() {
+            epochs += 1;
+
+
+            Parallel.For(0, trainMax, k => {
+                double addVar = 0;
+
+                /*
+                 * Start of feed forward calculations
+                 */
+
+                //Fill the truth array
+                for(int i = 0; i < outputLayerCount; i++) {
+                    truth[i] = 0;
+                }
+                truth[trainLabels[k]] = 1;
+
+                //Calculate the values of the nodes for the first layer
+                for(int i = 0; i < layer1count; i++) {
+                    S[i] = layer1BiasOld[i];
+                    for (int j = 0; j < inputLayerCount; j++) {
+                        //Parallel.For(0, 784, j => {
+                        S[i] += weight1Old[j, i] * trainImages[k, j];
+                    }
+                    firstLayer[i] = 1 / (1 + Math.Exp(-S[i]));
+                }
+
+                //Calculate the values the output 
+                for (int i = 0; i < outputLayerCount; i++) {
+                    outputLayer[i] = outLayerBiasOld[i];
+                    for (int j = 0; j < layer1count; j++) {
+                        outputLayer[i] += firstLayer[j] * weight2Old[j, i];
+                    }
+                    finalOutput[i] = 1 / (1 + Math.Exp(-outputLayer[i]));
+                }
+
+                /*
+                 * Start of back propagation calculations
+                 */
+
+                //fill the second weights
+                for (int i = 0; i < layer1count; i++) {
+                    for (int j = 0; j < outputLayerCount; j++) {
+                        weight2[i, j] = weight2Old[i, j] - learn * ((finalOutput[j] - truth[j])
+                        * finalOutput[j] * (1 - finalOutput[j]) * firstLayer[i]);
+                    }
+                }
+
+                //fill the first weights
+                for (int i = 0; i < inputLayerCount; i++) {
+                    for (int j = 0; j < layer1count; j++) {
+                        //Parallel.For(0, 50, j => {
+                        weight1[i, j] = weight1Old[i, j];
+                        addVar = 0;
+                        for (int b = 0; b < 10; b++) {
+                            addVar += (finalOutput[b] - truth[b]) * finalOutput[b] * (1 - finalOutput[b]) 
+                            * weight2Old[j, b] * firstLayer[j] * (1 - firstLayer[j]) * trainImages[k, i];
+                        }
+                        weight1[i, j] -= learn * addVar;
+                    }
+                }
+
+                //fill the layer one bias array
+                for (int i = 0; i < layer1count; i++) {
+                    layer1Bias[i] = layer1BiasOld[i];
+                    addVar = 0;
+                    for (int j = 0; j < 10; j++) {
+                        addVar += (finalOutput[j] - truth[j]) * finalOutput[j] * (1 - finalOutput[j]) 
+                        * weight2Old[i, j] * firstLayer[i] * (1 - firstLayer[i]);
+                    }
+                    layer1Bias[i] -= learn * addVar;
+                }
+
+                //fill the output layer bias array
+                for (int i = 0; i < outputLayerCount; i++) {
+                    outLayerBias[i] = outLayerBiasOld[i] - learn * 
+                    ((finalOutput[i] - truth[i]) * finalOutput[i] * (1 - finalOutput[i]));
+                }
+
+                //Fill the old arrays with new values
+                for (int i = 0; i < inputLayerCount; i++) {
+                    for (int j = 0; j < layer1count; j++) {
+                        weight1Old[i, j] = weight1[i, j];
+                    }
+                }
+
+                for (int i = 0; i < 50; i++) {
+                    for (int j = 0; j < 10; j++) {
+                        weight2Old[i, j] = weight2[i, j];
+                    }
+                }
+
+                for (int i = 0; i < 50; i++) {
+                    layer1BiasOld[i] = layer1Bias[i];
+                }
+
+                for (int i = 0; i < 10; i++) {
+                    outLayerBiasOld[i] = outLayerBias[i];
+                }
+
+
+            });    
+
         }
     }
 
